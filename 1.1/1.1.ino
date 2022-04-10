851df794f0dfc0bb8519a1e15bf1f11c6dc10163
MagnusBuzard <12kubek@gmail.com>
2022-04-10 18:35:43 +0200

    Update 1.1.ino

--- 1.1/1.1.ino
+++ 1.1/1.1.ino
@@ -1,3 +1,7 @@
+//Należy napisać program zapalający wbudowany na płytce Arduino LED po 1 s od startu
+//programu. Do opóźnienia można wykorzystać funkcję: delay(ms)
+//której argumentem jest opóźnienie w ms.
+
 void setup() 
 {
   pinMode(13, OUTPUT);
