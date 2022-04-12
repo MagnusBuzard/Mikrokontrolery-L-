e704f81100bc3d798590f4c90fd5cd331dfc8a0d
MagnusBuzard <12kubek@gmail.com>
2022-04-12 10:35:22 +0200

    Create Zadanie4.1.ino

---
+++ Kolokwium/Zadanie4.1.ino
@@ -0,0 +1,68 @@
+int LED1 = 13;
+int LED2 = 12;
+int LED3 = 11;
+int LED4 = 10;
+int S1 = A1;
+int S2 = A2;
+
+int C1 = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(LED1, OUTPUT);
+pinMode(LED2, OUTPUT);
+pinMode(LED3, OUTPUT);
+pinMode(LED4, OUTPUT);
+pinMode(S1, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+if(digitalRead(S2) == LOW){
+  C1=C1+1;
+  delay(350);
+}
+else if(digitalRead(S1) == LOW)
+{
+  C1=C1-1;
+  delay(350);
+}
+else{
+  C1=C1;
+}
+if(C1==0){
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED4, HIGH);
+}
+if(C1==1){
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED3, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED1, HIGH);
+}
+if(C1==2){
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED1, HIGH);
+}
+if(C1==3){
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED1, HIGH);
+}
+if(C1==4){
+  digitalWrite(LED4, LOW);
+  digitalWrite(LED3, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED1, LOW);
+}
+if(C1>=5){
+  C1=0;
+}
+}
